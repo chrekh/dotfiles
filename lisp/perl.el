27768d634bcb1b6a47d0f469089c7cace4862c22
Christer Ekholm <che@chrekh.se>
2013-06-17 01:12:46 +0200

    Don't use custom-set-variables

--- lisp/perl.el
+++ lisp/perl.el
@@ -2,27 +2,28 @@
 (defalias 'perl-mode 'cperl-mode)
  
 ;; Use 4 space indents via cperl mode
-(custom-set-variables
+(setq
  ;; Indentation
- '(cperl-indent-level 4)
- '(cperl-brace-offset 0)
- '(cperl-continued-brace-offset 0)
- '(cperl-label-offset 0)
- '(cperl-continued-statement-offset 0)
- '(cperl-close-paren-offset -4)
- '(cperl-indent-parens-as-block t)
+ cperl-indent-level 4
+ cperl-brace-offset 0
+ cperl-continued-brace-offset 0
+ cperl-label-offset 0
+ cperl-continued-statement-offset 0
+ cperl-close-paren-offset -4
+ cperl-indent-parens-as-block t
  
  ;; auto
- '(cperl-electric-parens nil)
- '(cperl-electric-keywords nil)
- '(cperl-auto-newline nil)
- '(cperl-electic-linefeed t)
- '(cperl-electric-lbrace nil)
- '(cperl-hairy nil)
+ cperl-electric-parens nil
+ cperl-electric-keywords nil
+ cperl-auto-newline nil
+ cperl-electic-linefeed t
+ cperl-electric-lbrace nil
+ cperl-hairy nil
+
+ ;; Insert spaces instead of tabs
+ indent-tabs-mode nil
 
  ;; other
- '(cperl-clobber-lisp-bindings nil)
+ cperl-clobber-lisp-bindings nil
  )
 
-;; Insert spaces instead of tabs
-(setq-default indent-tabs-mode nil)
