23c44261f57a424106ce676617583f5314700c99
Christer Ekholm <che@chrekh.se>
2014-09-09 20:32:03 +0200

    Improved handling of appending my lispdirs to load-path
    Test for existence of my lispdirs with a number of possible usernames
    (including ~) and add to load-path the expanded dirname.

--- lisp/emacs.el
+++ lisp/emacs.el
@@ -1,8 +1,13 @@
-(setq load-path (append
-		 '("~che/lisp" "~che/extern/lisp"
-		   "~chrekh/lisp" "~u0043002/lisp"
-		   "~u0043002/extern/lisp")
-		 load-path))
+(let ((dirs '("~/lisp" "~/extern/lisp"
+	      "~che/lisp" "~che/extern/lisp"
+	      "~chrekh/lisp" "~chrekh/extern/lisp"
+	      "~u0043002/lisp" "~u0043002/extern/lisp")))
+  (while dirs
+    (when (file-accessible-directory-p (car dirs))
+      (add-to-list 'load-path (expand-file-name (car dirs)) t))
+    (setq dirs (cdr dirs))))
+
+
 
 (load "loads")
 
