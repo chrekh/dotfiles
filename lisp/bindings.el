8c5ba7fce75f4cdf5283b43525e9fa6132e408e8
Christer Ekholm <che@chrekh.se>
2015-05-13 23:42:37 +0200

    Bind C-x v v to magit-status (if available)

--- lisp/bindings.el
+++ lisp/bindings.el
@@ -15,6 +15,8 @@
 
 (define-key text-mode-map "\e\t" 'lisp-complete-symbol)
 
+(cond ((fboundp 'magit-status)
+       (define-key global-map "\C-xvv" 'magit-status)))
 (cond ((>= emacs-major-version 21)
        (define-key global-map [home] 'beginning-of-buffer)
        (define-key global-map [end] 'end-of-buffer)
