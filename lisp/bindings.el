0cee075203f83211bdc618af5055c8af682e152c
Christer Ekholm <che@chrekh.se>
2015-05-21 23:01:29 +0200

    Removed unused bindings

--- lisp/bindings.el
+++ lisp/bindings.el
@@ -2,12 +2,10 @@
 (define-key global-map "\C-j" 'newline)
 (define-key global-map "\C-m" 'reindent-then-newline-and-indent)
 
-;(define-key global-map " " 'insert-or-scroll)
 (define-key global-map "\C-x\C-o" 'bury-buffer)
 (define-key global-map "\C-m" 'newline-and-indent)
 (define-key global-map "\C-d" 'delete-char-or-list)
 (define-key global-map "\C-u" 'delete-line)
-;(define-key global-map "\C-z" 'save-buffers-suspend)
 (define-key global-map "\C-cw" 'append-signature-long)
 (define-key global-map "\C-c\C-w" 'append-signature-short)
 (define-key global-map "\M-g" 'goto-line)
