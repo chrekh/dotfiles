d71558ebf8171c9d763cdf0e2f2e042f53f84de4
Christer Ekholm <Christer.Ekholm@skatteverket.se>
2020-07-30 09:12:08 +0200

    Only dump rsvm.se when in rsvm.se

--- rsv/bin/dumpdns.sh
+++ rsv/bin/dumpdns.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+domain=$(dnsdomainname)
+[ $domain ] || domain=none
+
+if [ $domain = 'rsvm.se' ]; then
+    # Only dump rsvm.se when in rsvm.se
+    host -al rsvm.se > $HOME/db/all-hosts
+    exit 0
+fi
+
 for d in acc.rsv.se. bmp.rsv.se. db.rsv.se. dmz.rsv.se. infra.rsv.se. ipa.rsv.se. kap.rsv.se. \
          labb.rsv.se. lb.rsv.se. meet.rsv.se. ocp.rsv.se. labb.ocp.rsv.se test.ocp.rsv.se. \
          prod.ocp.rsv.se ppx.rsv.se. pro.rsv.se. prov.rsv.se. rel1.rsv.se. rex.rsv.se. rsv.rsv.se. \
