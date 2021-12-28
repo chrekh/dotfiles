af0e48a84db8bf421bce2f8af41733bec4d8deb8
Christer Ekholm <che@chrekh.se>
2021-12-28 19:05:39 +0100

    Fix shellcheck warnings

--- rsv/bin/dumpdns.sh
+++ rsv/bin/dumpdns.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 domain=$(dnsdomainname)
-[[ $domain ]] || domain=none
+[[ ${domain} ]] || domain=none
 
-if [[ $domain = 'rsvm.se' ]]; then
+if [[ "${domain}" = 'rsvm.se' ]]; then
     # Only dump rsvm.se when in rsvm.se
-    host -al rsvm.se > $HOME/db/all-hosts
+    host -al rsvm.se > "${HOME}/db/all-hosts"
     exit 0
 fi
 
@@ -15,11 +15,9 @@ for d in acc.rsv.se. bmp.rsv.se. db.rsv.se. dmz.rsv.se. infra.rsv.se. ipa.rsv.se
          sys.rsv.se. test-dmz.rsv.se. test.rsv.se. utv.rsv.se. rsv.se. \
          skv.se test.skv.se. skvinfra.se. \
          val.se test.val.se. pro.val.se. ; do
-    host -al $d
-done > $HOME/db/all-hosts
+    host -al ${d}
+done > "${HOME}/db/all-hosts"
 
 # ibx.rsv.se don't allow zonetransfer for rsvm.se
-for d in rsvm.se; do
-    host -al $d v00001.rsvm.se
-done >> $HOME/db/all-hosts
-    
+host -al rsvm.se v00001.rsvm.se >> "${HOME}/db/all-hosts"
+
