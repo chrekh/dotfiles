4934736781d0af4c7dbdc59559450f2483ac1a37
Christer Ekholm <che@chrekh.se>
2022-04-29 23:06:58 +0200

    Allow multiple host arguments

--- bin/ssh-keyclean.bash
+++ bin/ssh-keyclean.bash
@@ -1,22 +1,22 @@
 #! /bin/bash
 
-h=$1
-[[ $h ]] || exit 1
-
 declare -A entries
-entries["$h"]=1
 
-IFS='
-'
-for line in $(/usr/bin/host "$h"); do
-  declare -a words
-  IFS=" " read -r -a words <<< "$line"
-  if [[ ${words[3]} == alias ]]; then
-    entries["${words[0]}"]=1
-  elif [[ ${words[2]} == address || ${words[3]} == address ]]; then
-    entries["${words[0]}"]=1
-    entries["${words[-1]}"]=1
-  fi
+for h in "$@"; do
+  entries["$h"]=1
+  
+  IFS='
+' 
+  for line in $(/usr/bin/host "$h"); do
+    declare -a words
+    IFS=" " read -r -a words <<< "$line"
+    if [[ ${words[3]} == alias ]]; then
+      entries["${words[0]}"]=1
+    elif [[ ${words[2]} == address || ${words[3]} == address ]]; then
+      entries["${words[0]}"]=1
+      entries["${words[-1]}"]=1
+    fi
+  done
 done
 
 for entry in "${!entries[@]}"; do
