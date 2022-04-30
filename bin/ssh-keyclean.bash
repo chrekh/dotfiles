620adae899b4b35c10425fc5f843af28692c542c
Christer Ekholm <che@chrekh.se>
2022-04-30 17:56:21 +0200

    Replace for-loop with while read loop
    To remove the need to set IFS

--- bin/ssh-keyclean.bash
+++ bin/ssh-keyclean.bash
@@ -5,18 +5,16 @@ declare -A entries
 for h in "$@"; do
   entries["$h"]=1
   
-  IFS='
-' 
-  for line in $(/usr/bin/host "$h"); do
+  while read -r line; do
     declare -a words
-    IFS=" " read -r -a words <<< "$line"
+    read -r -a words <<< "$line"
     if [[ ${words[3]} == alias ]]; then
       entries["${words[0]}"]=1
     elif [[ ${words[2]} == address || ${words[3]} == address ]]; then
       entries["${words[0]}"]=1
       entries["${words[-1]}"]=1
     fi
-  done
+  done < <(/usr/bin/host "$h")
 done
 
 for entry in "${!entries[@]}"; do
