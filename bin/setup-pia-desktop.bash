4fe4d096e27f753d0eca7fa5629811ee6bdfd6e3
Christer Ekholm <Christer.Ekholm@polisen.se>
2024-04-12 10:25:37 +0200

    Build fvwm3 from source

--- bin/setup-pia-desktop.bash
+++ bin/setup-pia-desktop.bash
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-cat > /etc/yum.repos.d/epel8.repo <<EOF
-[epel8]
-name=epel 8
-baseurl=http://satellite.appl.polisen.se/pulp/repos/Polisen/Library/custom/Extra_Packages_for_Enterprise_Linux/EPEL_8/
-enabled=0
-gpgcheck=0
-EOF
+if (( EUID == 0 )) ; then
+    echo "This is not supposed to run as root"
+    exit 1
+fi
 
-subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
+sudo cp ~/pkg/epel8.repo /etc/yum.repos.d
 
-yum --enablerepo=epel8 install ShellCheck emacs freetype-devel gcc gitk gitk libX11-devel libXcursor-devel libXft-devel libXpm-devel libXrandr-devel libXt-devel libevent-devel librsvg2-devel perl-LDAP perl-Sort-Versions perl-Tk perltidy readline-devel rxvt-unicode xauth xclock xev xloadimage xmodmap xterm yamllint
+sudo subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
+
+sudo yum --enablerepo=epel8 install ShellCheck emacs freetype-devel gcc gitk gitk libX11-devel libXcursor-devel libXft-devel libXpm-devel libXrandr-devel libXt-devel libevent-devel librsvg2-devel perl-LDAP perl-Tk perltidy readline-devel rxvt-unicode xauth xclock xev xmodmap xterm yamllint
+
+cd /tmp
+tar xf ~/X11/fvwm3-1.1.0.tar.gz
+cd /tmp/fvwm3-1.1.0
+./configure
+make -j3
+sudo make install
