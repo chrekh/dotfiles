159111656ad517796403442e402d2c92aa206455
Christer Ekholm <Christer.Ekholm@polisen.se>
2024-04-15 10:49:13 +0200

    Linit workaround for frame-size to Polisen (hostname match ^lx)

--- lisp/x-setup.el
+++ lisp/x-setup.el
@@ -20,11 +20,12 @@
 
 ;; Fix for problem with emacs-26.1 on RHEL8 under vcxsrv causing new frames to be resized to height 3
 ;; https://askubuntu.com/questions/1210236/how-to-stop-emacs-resizing-its-initial-frame
-(setq after-init-hook (lambda ()
-                        (set-frame-height (selected-frame) 37 ))
-      after-make-frame-functions (lambda (frame)
-                                   (set-frame-height frame 37 ))
-      )
+(when (string-match "^lx" (system-name))
+  (setq after-init-hook (lambda ()
+                          (set-frame-height (selected-frame) 37 ))
+        after-make-frame-functions (lambda (frame)
+                                     (set-frame-height frame 37 ))
+        ))
 
 (cond ((>= emacs-major-version 21)
        (tool-bar-mode -1)
