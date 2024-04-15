fd67486ebf37794c85b44694e23813c6f21ebc16
Christer Ekholm <Christer.Ekholm@polisen.se>
2024-04-15 10:46:35 +0200

    Revert "Use current window size to set frame size"
    This reverts commit 77897e9688e2ebb498ca4d77b4d8da0360ac6e39.
    
    No, this didn't help the problem at Polisen withHEL8 under vcxsrv

--- lisp/x-setup.el
+++ lisp/x-setup.el
@@ -21,9 +21,9 @@
 ;; Fix for problem with emacs-26.1 on RHEL8 under vcxsrv causing new frames to be resized to height 3
 ;; https://askubuntu.com/questions/1210236/how-to-stop-emacs-resizing-its-initial-frame
 (setq after-init-hook (lambda ()
-                        (set-frame-height (selected-frame) (window-height) ))
+                        (set-frame-height (selected-frame) 37 ))
       after-make-frame-functions (lambda (frame)
-                                   (set-frame-height frame (window-height) ))
+                                   (set-frame-height frame 37 ))
       )
 
 (cond ((>= emacs-major-version 21)
