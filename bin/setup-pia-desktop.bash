8940c647ee9b1ff0a00bcafdbbb1ebde4f5a2ae7
Christer Ekholm <Christer.Ekholm@polisen.se>
2025-01-14 07:41:36 +0100

    Add perl-Sort-Versions

--- bin/setup-pia-desktop.bash
+++ bin/setup-pia-desktop.bash
@@ -9,7 +9,7 @@ sudo cp ~/pkg/epel8.repo /etc/yum.repos.d
 
 sudo subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
 
-sudo yum --color=never --enablerepo=epel8 install ShellCheck emacs freetype-devel gcc gitk gitk libX11-devel libXcursor-devel libXft-devel libXpm-devel libXrandr-devel libXt-devel libevent-devel librsvg2-devel perl-LDAP perl-Tk perltidy readline-devel rxvt-unicode xauth xclock xev xmodmap xterm yamllint
+sudo yum --color=never --enablerepo=epel8 install ShellCheck emacs freetype-devel gcc gitk gitk libX11-devel libXcursor-devel libXft-devel libXpm-devel libXrandr-devel libXt-devel libevent-devel librsvg2-devel perl-LDAP perl-Tk perltidy readline-devel rxvt-unicode xauth xclock xev xmodmap xterm yamllint perl-Sort-Versions
 
 cd /tmp
 tar xf ~/X11/fvwm3-1.1.0.tar.gz
