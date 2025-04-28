22851725e24c679de0b7e4385ff92ab770d99997
Christer Ekholm <Christer.Ekholm@polisen.se>
2025-04-28 07:38:52 +0200

    Add support for Debian (Ubuntu)

--- bin/setup-pia-desktop.bash
+++ bin/setup-pia-desktop.bash
@@ -5,15 +5,24 @@ if (( EUID == 0 )) ; then
     exit 1
 fi
 
-sudo cp ~/pkg/epel8.repo /etc/yum.repos.d
+osfamily=$(facter os.family)
 
-sudo subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
+if [[ $osfamily == RedHat ]]; then
+  sudo cp ~/pkg/epel8.repo /etc/yum.repos.d
+  sudo subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
 
-sudo yum --color=never --enablerepo=epel8 install ShellCheck emacs freetype-devel gcc gitk gitk libX11-devel libXcursor-devel libXft-devel libXpm-devel libXrandr-devel libXt-devel libevent-devel librsvg2-devel perl-LDAP perl-Tk perltidy readline-devel rxvt-unicode xauth xclock xev xmodmap xterm yamllint perl-Sort-Versions
+  sudo yum --color=never --enablerepo=epel8 install ShellCheck emacs freetype-devel gcc gitk libX11-devel libXcursor-devel libXft-devel libXpm-devel libXrandr-devel libXt-devel libevent-devel librsvg2-devel perl-LDAP perl-Tk perltidy readline-devel rxvt-unicode xauth xclock xev xmodmap xterm yamllint perl-Sort-Versions
 
-cd /tmp
-tar xf ~/X11/fvwm3-1.1.0.tar.gz
-cd /tmp/fvwm3-1.1.0
-./configure
-make -j3
-sudo make install
+  cd /tmp || exit
+  tar xf ~/X11/fvwm3-1.1.0.tar.gz
+  cd /tmp/fvwm3-1.1.0 || exit
+  ./configure
+  make -j3
+  sudo make install
+
+elif [[ $osfamily == Debian ]]; then
+  sudo apt install fvwm3 shellcheck emacs gitk perl-doc libnet-ldap-perl perl-tk perltidy rxvt-unicode x11-apps x11-utils xterm yamllint x11-xserver-utils
+else
+  echo Unsupported osfamily "$osfamily"
+  exit 1
+fi
